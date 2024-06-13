905f9c13b6c39d7fd846e41a8615e717736d02a0
Nguyễn Văn Quyết <121377981+Paul-Greyrat@users.noreply.github.com>
2024-06-13 23:11:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ search.addEventListener('click', () => {
             temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
             description.innerHTML = `${json.weather[0].description}`;
             humidity.innerHTML = `${json.main.humidity}%`;
-            wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
+            wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`; 
 
             weatherBox.style.display = '';
             weatherDetails.style.display = '';
