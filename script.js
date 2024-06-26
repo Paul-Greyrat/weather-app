699a49ceede660f15ae66159adde467b80ffe4bb
Nguyễn Văn Quyết <121377981+Paul-Greyrat@users.noreply.github.com>
2024-06-26 16:59:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ search.addEventListener('click', () => {
 
     if (city === '')
         return; 
-
+ 
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}&units=metric&lang=vi`)
         .then(response => response.json())
         .then(json => {
